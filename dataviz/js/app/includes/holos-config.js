4dcfe1a728664b828b9810b2121b85915c8d0c7d
Brian Galey <bgaley@berkeley.edu>
2015-06-26 14:29:33 -0700

    Add config environ getter/setter

--- dataviz/js/app/includes/holos-config.js
+++ dataviz/js/app/includes/holos-config.js
@@ -1,17 +1,23 @@
 define(function () {
+  var _env = 'development';
 
 	var config = {
 		development : {
 			apiEndpoint : 'https://dev-ecoengine.berkeley.edu/api/',
-			tileserver : 'https://dev-ecoengine.berkeley.edu/api/tiles/'
+			tileURL : 'https://dev-ecoengine.berkeley.edu/api/tiles/'
 		},
 		production : {
-			apiEndpoint : 'https://ecoengine.berkeley.edu/api',
-			tileserver : 'https://ecoengine.berkeley.edu/api/tiles/'
+			apiEndpoint : 'https://ecoengine.berkeley.edu/api/',
+			tileURL : 'https://ecoengine.berkeley.edu/api/tiles/'
 		}
-	}
+	};
+
+  config.env = function(_) {
+    console.log('ENV:', _env);
+    if (!arguments.length) return config[_env];
+    _env = _;
+    return config[_env];
+  };
 
 	return config;
-
-
 });
