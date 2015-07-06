80a1ccc757593f42c54891df8038ab02958bbdb3
Brian Galey <bgaley@berkeley.edu>
2015-07-06 12:23:17 -0700

    Use singleton for config

--- dataviz/js/app/includes/config.js
+++ dataviz/js/app/includes/config.js
@@ -1,22 +1,25 @@
 define(function () {
-  var _env = 'development';
-
-	var config = {
-		development : {
-			apiEndpoint : 'https://dev-ecoengine.berkeley.edu/api/',
-			tileURL : 'https://dev-ecoengine.berkeley.edu/api/tiles/'
-		},
-		production : {
-			apiEndpoint : 'https://ecoengine.berkeley.edu/api/',
-			tileURL : 'https://ecoengine.berkeley.edu/api/tiles/'
-		}
-	};
-
-  config.env = function(_) {
-    if (!arguments.length) return config[_env];
-    _env = _;
-    return config[_env];
+  var envs = {
+    development: {
+      apiEndpoint : 'https://dev-ecoengine.berkeley.edu/api/',
+      tileURL : 'https://dev-ecoengine.berkeley.edu/api/tiles/'
+    },
+    production: {
+      apiEndpoint : 'https://ecoengine.berkeley.edu/api/',
+      tileURL : 'https://ecoengine.berkeley.edu/api/tiles/'
+    }
   };
 
-	return config;
+  var Config = function() {
+    this.env('development');
+  };
+
+  Config.prototype.env = function(_) {
+    if (!arguments.length) return this._env;
+    this._env = envs[_];
+    return this;
+  };
+
+  // Singleton
+  return new Config();
 });
